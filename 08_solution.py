40cefb0570fca5747bb1702dad26ea013f2a044f
Abhishek_thakur <sugheugbhbgvhure@gmail.com>
2026-04-06 16:40:56 +0530

    Delete 08_solution.py

--- 08_solution.py
+++
@@ -1,9 +0,0 @@
-def print_kwargs(**kwargs):
-    for key, value in kwargs.items():
-        print(f"{key}: {value}")
-
-print_kwargs(name = 'shaktiman', power = 'lezar')
-print_kwargs(name = 'shaktiman')
-print_kwargs(name = 'shaktiman', power = 'lezar')
-
-enemy = 'Dr. Jackaal'
