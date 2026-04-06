531bb71265376996f1cbc568dcb6a93da93547bd
Abhishek_thakur <sugheugbhbgvhure@gmail.com>
2026-04-06 16:31:35 +0530

    Add files via upload

---
+++ 01_solution.py
@@ -0,0 +1,12 @@
+age = input("Enter your age: ")
+
+age_in_int = int(age)
+
+if age_in_int < 13:
+    print("Child")
+elif age_in_int < 20:
+    print('Teenager')
+elif age_in_int < 60:
+    print('Adult')
+else:
+    print('senior')
---
+++ 05_solution.py
@@ -0,0 +1,12 @@
+weather = 'Rainy'
+
+if weather == 'Sunny':
+    activities = 'Go for a walk'
+elif weather == 'Rainy':
+    activities = 'Read a book'
+elif weather == 'Snowy':
+    activities = 'Snowman'
+else:
+    activities = 'No'
+
+print(activities)
